57965f0e3fc51f7178f365630bfefbf18a405dff
Jose Septien <jaseptienh@gmail.com>
2017-06-09 16:33:50 -0500

    Add main

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <libfreenect/libfreenect.hpp>
+#include <math.h>
+#include <pthread.h>
+#include "kinect.h"
+#include "chessBoard.h"
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
